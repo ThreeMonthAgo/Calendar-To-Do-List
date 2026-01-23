3ae951a49ae8ea8625be725e7abb1b07ddf46d09
linpigeon <pigeon-lin@outlook.com>
2026-01-23 22:51:56 +0800

    添加项目文件。

---
+++ CalenderToDoList/Models/ToDoTerm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CalenderToDoList.Models
+{
+    public class ToDoTerm
+    {
+        public bool IsDone { get; set; } = false;
+        public string TaskContent { get; set; } = string.Empty;
+        public DateTimeOffset? DeadLine { get; set; }
+    }
+}
---
+++ CalenderToDoList/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CalenderToDoList.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
