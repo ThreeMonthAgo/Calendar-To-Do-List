3db237048ce8d52579d2ded5785080b54490d50f
ThreeMonthAgo <225839283+ThreeMonthAgo@users.noreply.github.com>
2026-01-31 19:14:37 +0800

    Save todos

--- Calendar_To_Do_List/Calendar_To_Do_List/Services/ToDoService.cs
+++ Calendar_To_Do_List/Calendar_To_Do_List/Services/ToDoService.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.IO;
 using Calendar_To_Do_List.Utilities.DataType;
+using Calendar_To_Do_List.Utilities.Helpers;
 using Calendar_To_Do_List.Utilities.Interfaces;
+using Newtonsoft.Json;
 
 namespace Calendar_To_Do_List.Services;
 
 public class TodoService : ITodoService
 {
+    private static string FilePath => Path.Combine(IApp.DataDir, "todos.ctdltd");
     public ObservableCollection<TodoTerm> TodoTermCollection { get; set; } = [];
+
+    public TodoService()
+    {
+        LoadTodos();
+        TodoTermCollection.CollectionChanged += SaveTodos;
+    }
 
     public TodoTerm CreateTodo(string? summary, string? description, int priority, DateTime? dueDate = null)
     {
@@ -32,5 +44,28 @@ public class TodoService : ITodoService
     public void DeleteTodo(TodoTerm todo)
     {
         TodoTermCollection.Remove(todo);
+    }
+
+    private void SaveTodos(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        var c = JsonConvert.SerializeObject(TodoTermCollection);
+        ConfigurationHelper.SaveConfiguration(c, FilePath);
+    }
+
+    private void LoadTodos()
+    {
+        if (File.Exists(FilePath))
+        {
+            var s = ConfigurationHelper.LoadConfiguration(FilePath);
+            var todos = JsonConvert.DeserializeObject<ObservableCollection<TodoTerm>>(s);
+            if (todos is not null)
+            {
+                TodoTermCollection.Clear();
+                foreach (var t in todos)
+                {
+                    TodoTermCollection.Add(t);
+                }
+            }
+        }
     }
 }
